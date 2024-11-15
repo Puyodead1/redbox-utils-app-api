a5681b2e79da0bdf0f2d40e04091e7a635a131dc
Puyodead1 <puyodead@proton.me>
2024-11-15 18:04:23 -0500

    fix some weird crap on linux

--- main.py
+++ main.py
@@ -18,6 +18,26 @@ db = sqlite3.connect("profile.db", check_same_thread=False)
 cur = db.cursor()
 
 
+class InventoryItem:
+    def __init__(self, title_id: int, barcode: str, status_code: str):
+        self.title_id = title_id
+        self.barcode = barcode
+        self.status_code = status_code
+
+    def __str__(self):
+        return f"InventoryItem({self.title_id}, {self.barcode}, {self.status_code})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def to_dict(self):
+        return {"title_id": self.title_id, "barcode": self.barcode, "status_code": self.status_code}
+
+    @staticmethod
+    def from_construct(data):
+        return InventoryItem(data["title_id"], data["barcode"]["barcode"], data["status_code"])
+
+
 @app.route("/lookup/barcode/<barcode>", methods=["GET"])
 def lookup_barcode(barcode: str):
     print(f"Looking up barcode: {barcode}")
@@ -26,7 +46,7 @@ def lookup_barcode(barcode: str):
         item = archive.read_inventory(index)
         # strip total_rental_count
         item.pop("total_rental_count")
-        return jsonify(item)
+        return jsonify(InventoryItem.from_construct(item).to_dict())
     return jsonify({"error": "Item not found"}), 404
 
 
